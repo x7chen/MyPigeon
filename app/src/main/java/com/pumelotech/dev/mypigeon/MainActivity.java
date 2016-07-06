2fa7757d6bc0f0aaa909516a91d2ea27cef3fccc
x7chen <x7chen@126.com>
2016-07-06 21:41:14 +0800

    0706-2140

--- app/src/main/java/com/pumelotech/dev/mypigeon/MainActivity.java
+++ app/src/main/java/com/pumelotech/dev/mypigeon/MainActivity.java
@@ -1,87 +1,40 @@
 package com.pumelotech.dev.mypigeon;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.pumelotech.dev.mypigeon.BLE.PacketParser;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, PigeonListFragment.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity {
 
     public static final String DebugTag = "MyPigeon";
-    private MainPageFragment mainPageFragment;
-    private PigeonListFragment pigeonListFragment;
-    private HelpFragment helpFragment;
     private PacketParser mPacketParser;
     MyApplication myApplication;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_setting);
         setSupportActionBar(toolbar);
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-        // 设置默认的Fragment
-        setDefaultFragment();
+        Button BT_ManagerPigeon = (Button) findViewById(R.id.BT_managerPigeon);
+        BT_ManagerPigeon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, PigeonListActivity.class));
+            }
+        });
 
         mPacketParser = PacketParser.getInstance(this);
         MyPigeonDAO.getInstance(this);
     }
-    private void setDefaultFragment() {
-        setMainPageFragment();
-    }
 
-    private void setMainPageFragment() {
-        FragmentManager fm = getFragmentManager();
-        FragmentTransaction transaction = fm.beginTransaction();
-        mainPageFragment = new MainPageFragment();
-        transaction.replace(R.id.main_fragment_container, mainPageFragment);
-        transaction.commit();
-    }
-
-    private void setPigeonListFragment() {
-        FragmentManager fm = getFragmentManager();
-        FragmentTransaction transaction = fm.beginTransaction();
-        pigeonListFragment = new PigeonListFragment();
-        transaction.replace(R.id.main_fragment_container, pigeonListFragment);
-        transaction.commit();
-    }
-
-    private void setHelpFragment() {
-        FragmentManager fm = getFragmentManager();
-        FragmentTransaction transaction = fm.beginTransaction();
-        helpFragment = new HelpFragment();
-        transaction.replace(R.id.main_fragment_container, helpFragment);
-        transaction.commit();
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -89,7 +42,9 @@ public class MainActivity extends AppCompatActivity
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
     boolean Toggle;
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -97,38 +52,7 @@ public class MainActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }else if(id==R.id.menu_fly){
-
-            Toggle = !Toggle;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_pigeon) {
-            setMainPageFragment();
-        } else if (id == R.id.nav_fly) {
-            setPigeonListFragment();
-        } else if (id == R.id.nav_help) {
-            setHelpFragment();
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-
-    }
 }
---
+++ app/src/main/java/com/pumelotech/dev/mypigeon/PigeonListActivity.java
@@ -0,0 +1,78 @@
+package com.pumelotech.dev.mypigeon;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.pumelotech.dev.mypigeon.Adapter.PigeonListAdapter;
+import com.pumelotech.dev.mypigeon.DataType.PigeonInfo;
+
+import java.util.List;
+
+
+public class PigeonListActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pigeon_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_setting);
+        toolbar.setNavigationIcon(R.mipmap.ic_arrow_back_white_24dp);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        Button BT_addPigeon = (Button) findViewById(R.id.BT_addPigeon);
+        BT_addPigeon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PigeonListActivity.this,PigeonEditActivity.class));
+            }
+        });
+        ListView listView = (ListView) findViewById(R.id.listView_pigeon);
+        final PigeonListAdapter mPigeonListAdapter = new PigeonListAdapter(this);
+        listView.setAdapter(mPigeonListAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final PigeonInfo pigeon = mPigeonListAdapter.getPigeon(position);
+                if (pigeon == null) return;
+                Intent intent = new Intent(PigeonListActivity.this, RecordActivity.class);
+                intent.putExtra("Name", pigeon.Name);
+                startActivity(intent);
+            }
+        });
+        MyPigeonDAO myPigeonDAO = MyPigeonDAO.getInstance();
+        List<PigeonInfo> allPigeon = null;
+        if (myPigeonDAO != null) {
+            allPigeon = myPigeonDAO.getAllPigeon();
+        }
+        if (allPigeon != null) {
+            for (PigeonInfo pigeon : allPigeon) {
+                mPigeonListAdapter.addPigeon(pigeon);
+            }
+        }
+        mPigeonListAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.fly, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
